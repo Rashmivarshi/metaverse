33bb5545639c165f638288a160962ec15cf79c87
Rashmivarshi <rashmimaniampare@gmail.com>
2024-06-17 22:15:57 +0530

    quote commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { footerVariants } from "../utils/motion";
-import { socials } from "../constants";
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+import { socials } from '../constants';
 
 const Footer = () => (
   <motion.footer variants={footerVariants} initial="hidden" whileInView="show" className={`${styles.paddings} py-8 relative`}>
--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { fadeIn } from "../utils/motion";
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
 
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
